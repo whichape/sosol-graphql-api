0c19a75fef921fd65104f7686b2548e5cfd637c3
Harkl <3zy3@protonmail.com>
2021-09-02 13:05:46 +1200

    return signed

--- src/api/File/signUrl/signFileUrl.js
+++ src/api/File/signUrl/signFileUrl.js
@@ -3,8 +3,7 @@ const { getSignedS3URL } = require("../../../utils");
 module.exports = {
   Query: {
     signFileUrl: async (parent, args, ctx) => {
-      console.log(args.file);
-      return "xxx";
+      return getSignedS3URL({ key: args.file});
     },
   },
 };
