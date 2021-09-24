daae45b4d17224acc9a4b50431322cf37b7d53c7
Harkl <3zy3@protonmail.com>
2021-09-24 17:05:58 +1200

    format

--- terraform/12_task_definition.tf
+++ terraform/12_task_definition.tf
@@ -3,8 +3,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   family             = var.ecs_task_definition_name
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_role.arn
-  requires_compatibilities = [
-  "FARGATE"]
+  requires_compatibilities = ["FARGATE"]
   network_mode          = "awsvpc"
   cpu                   = 256
   memory                = 512
