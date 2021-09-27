635ee405c76df0ff2dfb1f9781a8b457bf67bf66
Harkl <3zy3@protonmail.com>
2021-09-27 14:02:40 +1300

    log database outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,14 @@ output "postgres_endpoint" {
   value = aws_db_instance.rds_instance.endpoint
 }
 
-output "alb-dns-name" {
+output "postgres_database_url" {
+  vale = "postgresql://${aws_db_instance.rds_instance.username}:${aws_db_instance.rds_instance.password}@${aws_db_instance.rds_instance.endpoint}:${aws_db_instance.rds_instance.port}/${aws_db_instance.rds_instance.name}?schema=public"
+}
+
+output "alb_dns_name" {
   value = aws_alb.alb.dns_name
+}
+
+output "jwt_secret" {
+  value = replace(random_string.sosol-secret-key.result, "\"", "")
 }
