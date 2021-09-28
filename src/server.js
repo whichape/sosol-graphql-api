be7cda61a5035dbbfdc87f4ac935cdcf2858c6d7
Harkl <3zy3@protonmail.com>
2021-09-28 17:38:19 +1300

    use apolloserver - seems to be good

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
 
 const schema = require("./schema");
-const { GraphQLServer } = require("graphql-yoga");
+const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 const { getUserId } = require("./utils");
 
 const prisma = new PrismaClient();
 
-const server = new GraphQLServer({
+const server = new ApolloServer({
   schema,
   context: (request) => ({ ...request, prisma, getUserId }),
 });
 
 const PORT = process.env.SOSOL_PORT || 7777;
 
-server.start(
+server.listen(
   {
     port: PORT,
   },
