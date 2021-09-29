d35e4aeaeaf2abd23d744d3be2d3a589370574b5
Harkl <3zy3@protonmail.com>
2021-09-29 13:26:18 +1300

    remove alarms

--- terraform/13_ecs_service.tf
+++ terraform/13_ecs_service.tf
@@ -3,8 +3,8 @@ resource "aws_ecs_service" "sosol-service" {
   cluster                            = aws_ecs_cluster.fp-ecs-cluster.id
   task_definition                    = aws_ecs_task_definition.task_definition.arn
   desired_count                      = 2
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
+  # deployment_minimum_healthy_percent = 50
+  # deployment_maximum_percent         = 200
   launch_type                        = "FARGATE"
   scheduling_strategy                = "REPLICA"
   enable_ecs_managed_tags            = true
