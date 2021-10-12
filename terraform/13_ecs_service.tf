4ba36c7122efabbf10c1c37fb49c1ab1753c6e76
Harkl <3zy3@protonmail.com>
2021-10-12 16:34:25 +1300

    tweak persistance and helath check

--- terraform/13_ecs_service.tf
+++ terraform/13_ecs_service.tf
@@ -2,8 +2,9 @@ resource "aws_ecs_service" "sosol_service" {
   name            = "sosol-app-service"
   cluster         = aws_ecs_cluster.fp_ecs_cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
-  desired_count   = 3
-  # deployment_minimum_healthy_percent = 50
+  desired_count   = 1
+  deployment_minimum_healthy_percent = 100
+  health_check_grace_period_seconds = 300
   # deployment_maximum_percent         = 200
   launch_type             = "FARGATE"
   scheduling_strategy     = "REPLICA"
