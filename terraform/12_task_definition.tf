34e48102f02d829444e34e7f9e727a04cb1dfd1d
Harkl <3zy3@protonmail.com>
2021-10-21 14:12:42 +1300

    bump cpu

--- terraform/12_task_definition.tf
+++ terraform/12_task_definition.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 1024
+  memory                   = 4096
   container_definitions    = data.template_file.task_definition_template.rendered
 }
